ef5d6a0a995abeb3d310666c5a8e8d191869039c
Maitha <77783725+maithaalghfeli@users.noreply.github.com>
2022-09-21 20:06:57 +0400

    Delete 30MFF directory

--- 30MFF/script.js
+++
@@ -1,8 +0,0 @@
-function myFunction(imgs) {
-  // Get the expanded image
-  var expandImg = document.getElementById("expandedImg");
-  // Use the same src in the expanded image as the image being clicked on from the grid
-  expandImg.src = imgs.src;
-  // Show the container element (hidden with CSS)
-  expandImg.parentElement.style.display = "block";
-}
