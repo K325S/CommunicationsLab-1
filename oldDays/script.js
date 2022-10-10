68ae28db809239fa2ef160b3be6b142e6d45aa27
Maitha <77783725+maithaalghfeli@users.noreply.github.com>
2022-10-10 09:15:38 +0400

    Add files via upload

---
+++ oldDays/script.js
@@ -0,0 +1,25 @@
+// ALL THESE FUNCTIONS ARE THE SAME, THE ONLY DIFFERENCE IS THE ALLOCATION OF CLASS TO ITEM.
+$(document).ready(function(){
+  $(".star1").click(function(){
+    // SLIDE TOGGLE USED TO ALLOW CLICKING AND UNCLICKING AN IMAGE
+    $(".chapter1").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $(".star2").click(function(){
+    $(".chapter2").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $(".star3").click(function(){
+    $(".chapter3").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $(".star6").click(function(){
+    $(".chapter6").slideToggle("slow");
+  });
+});
